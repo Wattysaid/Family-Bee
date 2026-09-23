8f3ba3cb526de59c95de06cd7d37e9e55af1845d
Wattysaid <kieran.francis.noonan@gmail.com>
2026-09-23 13:02:49 +0100

    feat: add config.example.js

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js and add your browser OAuth client ID.
+// The client ID is public configuration; never put a client secret in this file.
+window.FAMILY_BEE_CONFIG = {
+  googleClientId: 'YOUR_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com'
+};
+
